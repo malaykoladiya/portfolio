58dd881c5aa6128140613c76ffc397ec68854895
Malay Koladiya <malaykoladiya@gmail.com>
2024-05-28 20:03:10 -0700

    implemented vercel analytics and insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "@/components/transitionProvider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <TransitionProvider>{children}</TransitionProvider>
+        <TransitionProvider>
+          {children}
+          <SpeedInsights />
+          <Analytics />
+        </TransitionProvider>
       </body>
     </html>
   );
